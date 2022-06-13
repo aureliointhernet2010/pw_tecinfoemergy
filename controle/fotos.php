b0d6ac5e8f6a5051475c259caf4085a8093aa2b2
Aurelio <aurelio2013silva@gmail.com>
2022-06-13 10:19:31 -0300

    fotos.php implementado

---
+++ controle/fotos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload de fotos</title>
+</head>
+<body>
+    <h1>Faça o upload de fotos aqui!</h1>
+
+    <form method="post" action="">
+    <input type="file" name="imagem" id="imagem"><br><br>
+    <img style="width: ;" alt="">
+
+    <input type="submit" value="Enviar foto">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <script>
+        function previewImage(){
+            var imagem = document.querySelector('input[name= imagem]');
+            var preview = document.querySelector
+        }
+    </script>
+
+    </form>
+</body>
+</html>
