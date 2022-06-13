1434f67ff5d24cc56943eb35d67de201e03b4d09
anavidall312 <anaceciliavidal538@gmail.com>
2022-06-13 10:13:05 -0300

    login implementado

--- controle/conexao.php
+++ controle/conexao.php
@@ -4,7 +4,7 @@ $conexao = null;
 
 try{
     $conexao = new PDO('mysql:host=localhost;dbname=cadastropbl', 'root', '');
-    echo "Conectado com sucesso";
+    // echo "Conectado com sucesso";
 }catch(Exception $e) {
     echo $e->getMessage();
     die();
