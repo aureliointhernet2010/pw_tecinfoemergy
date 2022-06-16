7fb4c4f79103806c28fe81d6f773e510d039b15e
aureliointhernet2010 <103606784+aureliointhernet2010@users.noreply.github.com>
2022-06-16 18:41:40 -0300

    Update login.php

--- controle/login.php
+++ controle/login.php
@@ -1,11 +1,17 @@
 <?php
+
+//Sistema de identificação dos usuários cadastrados ao efetuar o login
+
+
 $pdo = require 'conexao.php';
 $sql = 'select * from usuarios';
 
+//Pesquisa
 $pesquisa = $pdo->query($sql);
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
+//Caso o usuário não seja cadastrado
 $login = false;
 
 foreach ($pesquisa as $key => $valor) {
@@ -18,3 +24,6 @@ if ($login == true) {
 } else {
     header('location:../pages/login.php');
 }
+//fim
+?>
+
