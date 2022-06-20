520c2e68a5c0c002ebaaa3c600708b1b595b21f0
aureliointhernet2010 <103606784+aureliointhernet2010@users.noreply.github.com>
2022-06-20 09:02:12 -0300

    Update conexao.php

--- controle/conexao.php
+++ controle/conexao.php
@@ -2,12 +2,14 @@
 
  //conexao dos formulários com banco de dados
 
+//aqui há uma variavel sem valor
 $conexao = null;
 
 //fazendo a conexao com o banco
 try{
+ //tratando execeções caso tenha erros ao conectar 
     $conexao = new PDO('mysql:host=localhost;dbname=cadastropbl', 'root', '');
-    // echo "Conectado com sucesso";
+    
     //caso aconteça algum erro de conexao
 }catch(Exception $e) {
     echo $e->getMessage();
